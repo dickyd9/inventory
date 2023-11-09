02115a03156ff273845c801a08dd009f18d5dc25
dickyd9 <dickyd6@gmail.com>
2023-11-09 14:57:09 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,11 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: LoggerFactory('Logging'),
-    cors: true,
+    // cors: true,
   });
   const corsOptions: CorsOptions = {
     origin: '*',
-    credentials: false,
-    // origin: [
-    //   'http://127.0.0.1:5173',
-    //   'http://127.0.0.1:5174',
-    //   'https://654bcd947422180008e3e2d7--digpos.netlify.app',
-    //   '*'
-    // ],
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    // credentials: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   };
   app.enableCors(corsOptions);
 
